2c8da7e4261db726cdbf7d7d86af39d95a2f6d91
huangugua <714309761@qq.com>
2021-07-06 21:56:42 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/css/global.css'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Element)
+
+import axios from 'axios'
+// 配置请求的路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
