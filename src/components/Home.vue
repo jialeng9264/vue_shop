c7fcf1dae3e509b7d00463941a0f8c871a1cc84e
huangugua <714309761@qq.com>
2021-07-07 23:02:14 +0800

    左侧菜单栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,130 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
+  <div id="container">
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+      <el-header>
+        <div>
+          <img src="../assets/img/cart.png" alt="">
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <!-- 页面主题区域 -->
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside :width="isCollapse ? '64px' : '200px' ">
+          <div class="toggle-button" @click="toggleCollapse">|||</div>
+          <el-menu
+            background-color="#333744"
+            text-color="#fff"
+            active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+            <!-- 一级菜单 -->
+            <el-submenu :index="item.id + ''" 
+              v-for="item in menulist" 
+              :key="item.id"
+              >
+              <!-- 一级菜单的模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>{{item.authName}}</span>
+              </template>
+              <!-- 二级菜单 -->
+              <el-menu-item :index=" '/' + subitem.path" 
+                v-for="subitem in item.children"
+                :key="subitem.id">
+                <template slot="title">
+                  <!-- 图标 -->
+                  <i class="el-icon-menu"></i>
+                  <!-- 文本 -->
+                  <span>{{subitem.authName}}</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>
+          <!-- 路由占位符 -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      // 左侧菜单数据
+      menulist:[],
+      // 是否这折叠
+      isCollapse:false
+    }
+  },
+  created(){
+    this.getMenuList()
+  },
   methods:{
     logout(){
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList(){
+      const {data:res} = await this.$http.get('menus')
+      if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮，切换菜单的折叠与展开
+    toggleCollapse(){
+      this.isCollapse = ! this.isCollapse
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+  #container{
+    height: 100%;
+  }
+  .home-container{
+    height: 100%;
+  }
+  .el-header{
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    background-color: #373d41;
+    > div{
+      display: flex;
+      align-items: center;
+        span{
+          margin-left: 15px;
+        }
+    }
+  }
+  .el-aside{
+    background-color: #333744;
+      .el-menu{
+        border-right: none;
+      }
+  }
+  .el-main{
+    background-color: #eaedf1;
+  }
+  .toggle-button{
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
 </style>
